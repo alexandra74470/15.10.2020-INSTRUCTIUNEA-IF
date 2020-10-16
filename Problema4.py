1bcb74a6c01bb870cfdf9885ff76b064edf76bff
Alexandra Juravets <70714347+alexandra74470@users.noreply.github.com>
2020-10-16 21:39:15 +0300

    Add files via upload
    Se introduc varstele a 3 persoane. Afisati varstele cuprins intre 18 si 60 de ani.

---
+++ Problema4.py
@@ -0,0 +1,9 @@
+a=int(input('varsta primei persoane'))
+b=int(input('varsta persoanei a doua'))
+c=int(input('varsta persoanei a treia'))
+if (a>18)and (a<60):
+    print(a)
+if(b>18)and(b<60):
+    print(b)
+if(c>18)and(b<60):
+    print(c)
