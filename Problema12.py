efaacaa95bd032b7218894926b4bd8bd0f868d2a
Alexandra Juravets <70714347+alexandra74470@users.noreply.github.com>
2020-10-18 18:25:09 +0300

    Add files via upload
    “Mă iubeşte un pic, mult, cu pasiune, la nebunie, de loc, un pic,…”. Rupând petalele unei margarete cu x petale, el (ea) mă
    iubeşte ….

---
+++ Problema12.py
@@ -0,0 +1,11 @@
+x=int(input('introduceti numarul de petale'))
+if(x%5==1):
+    print('ma iubese un pic')
+if(x%5==2):
+    print('ma iubeste mult')
+if(x%5==3):
+    print('ma iubeste cu pasiune')
+if(x%5==4):
+    print('ma iubeste la nebunie')
+else:
+    print('nu ma iubeste deloc')
